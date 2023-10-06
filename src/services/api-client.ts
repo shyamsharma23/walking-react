7242d008fc76dadfc4d715adfabce266de5718bf
SHYAM  SHARMA <shyamsharma@shyams-air.mynetworksettings.com>
2023-10-05 20:16:00 -0400

    commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: 'http://ec2-54-208-26-38.compute-1.amazonaws.com:3000/api/v1',
 
 })
